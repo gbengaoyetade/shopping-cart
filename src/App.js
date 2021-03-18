22d7131783ce7f3a464f6729faf2085557ec91e4
Gbenga Oyetade <ioyetade@gmail.com>
2021-03-18 12:34:00 +0100

    fix mobile styles

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useQuery, gql } from '@apollo/client';
 import { ProductList } from './components/ProductList';
 import styles from './App.module.css';
 import { Cart } from './components/Cart';
+import loadingGif from './assets/images/loading.gif';
 
 const getProductsQuery = (currency) => {
   return gql`
@@ -22,11 +23,20 @@ const App = (props) => {
 
   const { loading, error, data } = useQuery(getProductsQuery(currency));
 
-  if (loading) return <p>Loading...</p>;
+  if (loading)
+    return (
+      <div className={styles['loading-wrapper']}>
+        <img src={loadingGif} alt='Loading gif' />
+      </div>
+    );
   if (error) return <p>Error</p>;
 
   return (
     <div className={styles.wrapper}>
+      <header className={styles.header}>
+        <h1>All Products</h1>
+        <p>A 360&#730; look at Lumin</p>
+      </header>
       <Cart currency={currency} setCurrency={setCurrency} />
       <ProductList products={data.products} currency={currency} />
     </div>
